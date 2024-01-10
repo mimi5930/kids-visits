b3221b1384228efc13e9e74582958dd8d2a284ae
Mike <mikej.miller440@gmail.com>
2024-01-10 14:53:26 -0600

    created vite project

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        reference: resolve(__dirname, 'references.html'),
+        'js/script': resolve(__dirname, './assets/js/script.js')
+      },
+      output: {
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`
+      }
+    }
+  }
+})
