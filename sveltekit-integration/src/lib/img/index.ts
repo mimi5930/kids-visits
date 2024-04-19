3c7cee0b17d56866938eb5813c6869ca847cb322
Mike Miller <mikej.miller440@gmail.com>
2024-04-18 22:33:53 -0500

    added index for images

---
+++ sveltekit-integration/src/lib/img/index.ts
@@ -0,0 +1,27 @@
+import austinPacheco from './austin-pacheco-FtL07GM9Q7Y-unsplash-min.jpg';
+import benWhite from './ben-white-JJ9irt1OZmI-unsplash-min.jpg';
+import brenProfile from './bren-profile-min.jpg';
+import chayeneRafaela from './chayene-rafaela-nGwkIZFelko-unsplash-min.jpg';
+import derekThomson from './derek-thomson-min.jpg';
+import ekaterinaShakharova from './ekaterina-shakharova-L4nwL3195U0-unsplash-min.jpg';
+import hannaTasker from './hannah-tasker-ZBkH8G4_yyE-unsplash-min.jpg';
+import kellySikkema from './kelly-sikkema-min.jpg';
+import kevinDelvecchio from './kevin-delvecchio-7noZJ_4nhU8-unsplash-min.jpg';
+import mTElgassier from './m-t-elgassier-min.jpg';
+import nathanDumlao from './nathan-dumlao-min.jpg';
+import paulHanaoka from './paul-hanaoka-a104tlUezug-unsplash-min.jpg';
+
+export {
+	austinPacheco,
+	benWhite,
+	brenProfile,
+	chayeneRafaela,
+	derekThomson,
+	ekaterinaShakharova,
+	hannaTasker,
+	kellySikkema,
+	kevinDelvecchio,
+	mTElgassier,
+	nathanDumlao,
+	paulHanaoka
+};
