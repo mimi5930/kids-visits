2fac9c01da9883c3aef8785a50604ef3b5ea154c
Mike Miller <mikej.miller440@gmail.com>
2024-04-19 01:14:19 -0500

    added expandible navbar

--- sveltekit-integration/src/components/Header.svelte
+++ sveltekit-integration/src/components/Header.svelte
@@ -1,43 +1,58 @@
 <script lang="ts">
-	import { Nav, Navbar, NavItem, NavLink, NavbarBrand } from '@sveltestrap/sveltestrap';
+	import {
+		Nav,
+		Navbar,
+		NavItem,
+		NavLink,
+		NavbarBrand,
+		NavbarToggler,
+		Collapse
+	} from '@sveltestrap/sveltestrap';
+	let isOpen = false;
+	function handleUpdate(event: CustomEvent<boolean>) {
+		isOpen = event.detail;
+	}
 </script>
 
-<Navbar>
+<Navbar expand="md">
 	<NavbarBrand href="/" class="page-title">Brenda Jacobson</NavbarBrand>
-	<Nav>
-		<NavItem>
-			<NavLink href="/">Home</NavLink>
-		</NavItem>
-		<NavItem>
-			<NavLink href="/">References</NavLink>
-		</NavItem>
-	</Nav>
+	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
+	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+		<Nav class="ms-auto" navbar>
+			<NavItem>
+				<NavLink href="/">Home</NavLink>
+			</NavItem>
+			<NavItem>
+				<NavLink href="/">References</NavLink>
+			</NavItem>
+		</Nav>
+	</Collapse>
 </Navbar>
 
 <style>
-:global(.page-title) {
+	:global(.page-title) {
 		font-family: var(--web-title-font);
 		font-size: 30px;
 		color: black;
 		transition: 0.3s;
 	}
-:global(.page-title:hover) {
-	color: black;
-	opacity: 60%;
-}
-:global(.navbar) {
-  background-color: var(--nav-color);
-}
+	:global(.page-title:hover) {
+		color: black;
+		opacity: 60%;
+	}
+	:global(.navbar) {
+		background-color: var(--nav-color);
+	}
 
-:global(.nav-link) {
-  color: black;
-  font-size: 20px;
-  font-weight: bold;
-  transition: 0.3s;
-}
+	:global(.nav-link) {
+		color: black;
+		font-size: 20px;
+		font-weight: bold;
+		transition: 0.3s;
+	}
 
-:global(.nav-link:hover) {
-  color: black;
-	opacity: 60%;
-}
+	:global(.nav-link:hover) {
+		color: black;
+		opacity: 60%;
+	}
 </style>
