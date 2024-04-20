302f61bafb58d20588ca34ebe4b6ccc579fb308a
Mike Miller <mikej.miller440@gmail.com>
2024-04-20 16:25:08 -0500

    changed svelte integration to main dir

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,8 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			appDir: 'app' // Required as the default is _app
-			// fallback: '404.html' // may differ from host to host
+			appDir: 'app', // Required as the default is _app
+			fallback: '404.html' // may differ from host to host
 		})
 	}
 };
